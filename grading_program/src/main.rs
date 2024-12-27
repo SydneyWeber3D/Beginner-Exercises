c84b64d966d3c427b3085a6597809c7c85c61e97
Sydney Weber <SydneyWeber3D@gmail.com>
2024-12-26 19:42:56 -0500

    Implemented first part of Grading Program.

--- grading_program/src/main.rs
+++ grading_program/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdin, stdout, Write};
+
+fn main()
+{
+	loop
+	{
+		print!("Enter your grade (0-100): ");
+		let _ = stdout().flush();
+
+		let mut raw_user_input: String = String::new(); 
+		stdin().read_line(&mut raw_user_input).expect("Incorrect input.");
+
+		let user_input: u8 = raw_user_input.trim().parse().expect("You must enter a number between 0 and 100.");
+
+		if user_input == 100
+		{
+			println!("Congratulations, that's a perfect score!");
+		}
+	}
 }
