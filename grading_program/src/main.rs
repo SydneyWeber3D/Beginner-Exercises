755c7df1f91496ff228b5f1e345fe826aada544f
Sydney Weber <SydneyWeber3D@gmail.com>
2024-12-26 19:51:44 -0500

    Implemented second part of Grading Program.

--- grading_program/src/main.rs
+++ grading_program/src/main.rs
@@ -4,17 +4,26 @@ fn main()
 {
 	loop
 	{
-		print!("Enter your grade (0-100): ");
+		print!("Enter your grade (0-100), or Q to quit: ");
 		let _ = stdout().flush();
 
 		let mut raw_user_input: String = String::new(); 
 		stdin().read_line(&mut raw_user_input).expect("Incorrect input.");
+
+		if raw_user_input.trim() == "q" || raw_user_input.trim() == "Q"
+		{
+			break;
+		}
 
 		let user_input: u8 = raw_user_input.trim().parse().expect("You must enter a number between 0 and 100.");
 
 		if user_input == 100
 		{
 			println!("Congratulations, that's a perfect score!");
+		}
+		else if user_input < 100 && user_input >=90
+		{
+			println!("Great job, that's a solid A!");
 		}
 	}
 }
