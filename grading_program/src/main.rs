b55e017ca77f16ed9cbb0268f9163a512ccf5fa0
Sydney Weber <SydneyWeber3D@gmail.com>
2024-12-28 16:24:15 -0500

    Added comments and exercise guidelines to Grading Program Added comments and exercise guidelines to Soda Machine

--- grading_program/src/main.rs
+++ grading_program/src/main.rs
@@ -1,22 +1,42 @@
+/*
+	Write a program that allows the user to enter the grade scored in a programming class (0-100)
+	If the user scored a 100 then notify the user that they got a perfect score
+
+	★ Modify the program so that if the user scored a 90-100 it informs the user that they scored an A
+
+	★★ Modify the program so that it will notify the user of their letter grade
+	0-59 F, 60-69 D, 70-79 C, 80-89 B, 90-100 A
+*/
+
 use std::io::{stdin, stdout, Write};
 
 fn main()
 {
+	// Program loops infinitely until user input is invalid or user inputs to quit
+	// No error handling, basic exercise
 	loop
 	{
+		// Instruct user to provide input within certain parameters
 		print!("Enter your grade (0-100), or Q to quit: ");
+		// Flush stdout buffer for clean stdin slate
 		let _ = stdout().flush();
 
+		// Take in raw user input
 		let mut raw_user_input: String = String::new(); 
 		stdin().read_line(&mut raw_user_input).expect("Incorrect input.");
 
+		// Break out of the loop if the user input matches quitting
+		// Trim to remove excess unwanted characters
 		if raw_user_input.trim() == "q" || raw_user_input.trim() == "Q"
 		{
 			break;
 		}
 
+		// Convert user input to integer form for easier comparison
+		// Expected input between 0 and 100, u8 is more than enough
 		let user_input: u8 = raw_user_input.trim().parse().expect("You must enter a number between 0 and 100.");
 
+		// Print out different results based on user input
 		if user_input == 100
 		{
 			println!("Congratulations, that's a perfect score!");
--- soda_machine/src/main.rs
+++ soda_machine/src/main.rs
@@ -1,3 +1,14 @@
+/*
+	Write a program that presents the user w/ a choice of your 5 favorite beverages (Coke, Water, Sprite...)
+	Then allow the user to choose a beverage by entering a number 1-5
+	Output which beverage they chose
+
+	★ If you program uses if statements instead of a switch statement, modify it to use a switch statement
+	If instead your program uses a switch statement, modify it to use if/else-if statements
+
+	★★ Modify the program so that if the user enters a choice other than 1-5 then it will output "Error. choice was not valid, here is your money back."
+*/
+
 use std::io::{stdin, stdout, Write};
 
 fn main()
@@ -6,26 +17,36 @@ fn main()
 
 	loop
 	{
+		// List and loop possible choices so the user always knows their options
 		println!("  1) Pempis");
 		println!("  2) Conke");
 		println!("  3) Sproot");
 		println!("  4) Ep Pepepe");
 		println!("  5) Funti");
 
+		// Instruct user to provide input within certain parameters
 		print!("\nPick your poison, or Q to quit: ");
+		// Flush stdout buffer for clean stdin slate
 		let _ = stdout().flush();
 
+		// Take in raw user input
 		let mut raw_user_input: String = String::new(); 
 		stdin().read_line(&mut raw_user_input).expect("Incorrect input.");
 
+		// Break out of the loop if the user input matches quitting
+		// Trim to remove excess unwanted characters
 		if raw_user_input.trim() == "q" || raw_user_input.trim() == "Q"
 		{
 			break;
 		}
 
+		// Convert user input to integer form for easier comparison
+		// Expected input between 0 and 100, u8 is more than enough
 		let user_input: u8 = raw_user_input.trim().parse().expect("You must enter a number between 0 and 5.");
 
-		/*if user_input == 1
+		// Initial implementation
+		/*
+		if user_input == 1
 		{
 			println!("Dispensing can of Pempis...");
 		}
@@ -48,8 +69,10 @@ fn main()
 		else
 		{
 			println!("That's not an option...");
-		}*/
+		}
+		*/
 
+		// Part 2 implementation, preferrable over IF statements
 		match user_input
 		{
 			1 => println!("Dispensing can of Pempis...\n"),
